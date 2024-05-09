6ef1dd26a5d41f1f7665fadb12374f47651a1028
DerekNoe <148905808+DerekNoe@users.noreply.github.com>
2024-05-09 09:09:34 -0400

    completed exercises for wine reviews

--- reviews.py
+++ reviews.py
@@ -1,2 +1,10 @@
-# add your code here
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\dnoe1\OneDrive\Documents\Projects\Wine\wine-reviews-exercise-DerekNoe\data\winemag-data-130k-v2.csv.zip", compression="zip")
+
+grouped_df = df.groupby('country').agg(count=('points', 'count'), average_points=('points', 'mean'))
+grouped_df.reset_index(inplace=True)
+final_df = grouped_df.set_index("country").sort_values(by="count", ascending=False)
+
+final_df.to_csv("reviews-per-country.csv", sep=",", encoding="utf-8")
 
